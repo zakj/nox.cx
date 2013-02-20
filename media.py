067d59a393f28e72acd650423edc3ad1c209433b
Zak Johnson <zakj@nox.cx>
2013-02-20 14:47:17 -0800

    Clean up imports

--- media.py
+++ media.py
@@ -3,17 +3,18 @@ import calendar
 from instagram.client import InstagramAPI
 from flask.ext.script import Command
 
-from nox import app, redis
-
 
 class CollectLatestMedia(Command):
     """Collects the latest Instagram images."""
 
-    def run(self):
+    def handle(self, app):
+        return self.run(app)
+
+    def run(self, app):
         api = InstagramAPI(access_token=app.config['INSTAGRAM_ACCESS_TOKEN'])
         user_id = app.config['INSTAGRAM_USER_ID']
         media, _next = api.user_recent_media(user_id=user_id, count=10)
-        p = redis.pipeline(transaction=False)
+        p = app.redis.pipeline(transaction=False)
         for item in media:
             p.hmset('instagram:media:%s' % item.id, dict(
                 thumbnail=item.images['thumbnail'].url,
